858d7ecda754c3d3be8428b69cf93469d2c25f33
Ryan Pitts <rmpittshteam@gmail.com>
2018-12-18 17:42:46 -0500

    Display pace in hours, minutes, and seconds.

--- PaceCalculator.py
+++ PaceCalculator.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 """ This calculator determines pace, time, or distance. """
+from datetime import time, timedelta
 
 def get_input():
     values = {
-        "time": 0,
+        "time": "",
         "distance": 0
     }
 
-    values["time"] = int(input("Enter expected time in minutes:  "))
+    values["time"] = input("Enter expected time in hours, minutes, and seconds (HH:MM:SS):  ")
     values["distance"] = int(input("Enter distance in miles:  "))
     
     return values
 
 def calculate_pace(user_values):
-    return user_values.get("time") / user_values.get("distance")
+    secs_per_dist = convert_input_to_seconds(user_values.get("time")) / user_values.get("distance")
+    return timedelta(seconds=secs_per_dist)
+
+def convert_input_to_seconds(time_input):
+    t = time.fromisoformat(time_input)
+    secs = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second).total_seconds()
+    return secs
 
 user_values = get_input()
-print("Pace = " + str(calculate_pace(user_values)) + " mins/mile")
+pace_time = calculate_pace(user_values)
+print("Pace = " + str(pace_time) + " per mile")
